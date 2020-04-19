2dad6275e812fff23acd5f67fc427fd164c3b77b
HajimeNoJosh <joshuah.m.martin@gmail.com>
2020-04-18 22:13:45 -0400

    fixining the this issue

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ class Timer {
         this.startButton.addEventListener('click', this.start)
     }
 
-    start() {
-        console.log('Time to start the timer!')
+    start = () => {
     }
 }
 
@@ -17,3 +16,4 @@ const startButton = document.querySelector('#start')
 const pauseButton = document.querySelector('#pause')
 
 const timer = new Timer(durationInput, startButton, pauseButton)
+timer.start()
