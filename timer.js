0908f812c0eba465ef54b5e9c1324142cc7918d9
HajimeNoJosh <joshuah.m.martin@gmail.com>
2020-04-19 00:06:11 -0400

    emitting messages for border, disable buttons pause and start respectivly, cleaning up the code

---
+++ timer.js
@@ -0,0 +1,54 @@
+class Timer {
+  constructor(durationInput, startButton, pauseButton, callbacks) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
+    }
+
+    this.startButton.addEventListener("click", this.start);
+    this.pauseButton.addEventListener("click", this.pause);
+  }
+
+  start = () => {
+    if (this.onStart) {
+      this.onStart();
+    }
+    this.tick();
+    this.intervalId = setInterval(this.tick, 1000);
+    startButton.disabled = true;
+    pauseButton.disabled = false;
+  };
+
+  tick = () => {
+    if (this.timeRemaining <= 0) {
+      this.pause();
+      if (this.onComplete) {
+        this.onComplete();
+      }
+    } else {
+      this.timeRemaining = this.timeRemaining - 1;
+      if (this.onTick) {
+        this.onTick();
+      }
+    }
+  };
+
+  pause = () => {
+    clearInterval(this.intervalId);
+    startButton.disabled = false;
+    pauseButton.disabled = true;
+  };
+
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+
+  set timeRemaining(time) {
+    this.durationInput.value = time;
+  }
+}
